ca7fc58ada42a852ee5c7d925aae8cfb2e6aab79
Kein <kein.carrillo.escom@gmail.com>
2025-05-22 17:00:57 -0600

    feat<App.tsx>: Componente IPinputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "@/components/Header";
-import SubnetInput from "@/components/SubnetInput";
+//import SubnetInput from "@/components/SubnetInput";
+import IPinputs from "@/components/IPinputs";
 
 const App: React.FC = () => {
   return (
     <>
       <Header title={"Calculadora de Subneteo"} />
       <main className="w-4/5 m-auto h-screen bg-primary font-primary grid grid-cols-2">
-        <SubnetInput />
+        <IPinputs />
       </main>
     </>
   );
