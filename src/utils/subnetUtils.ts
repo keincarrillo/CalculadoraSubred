934cc9a40226c0b4e319d3e04d06d49f6085db0d
Kein <kein.carrillo.escom@gmail.com>
2025-05-22 16:59:09 -0600

    refactor<SubnetInput.tsx>: Organizacion de funciones

--- src/utils/subnetUtils.ts
+++ src/utils/subnetUtils.ts
@@ -1,9 +1,8 @@
 export function isSubnetInvalid(mask: string, newMask: string): boolean {
-  const numericMask = Number(mask);
+  const numericMask = Number(mask); // Casteo
   const numericNewMask = Number(newMask);
 
   if (isNaN(numericMask) || isNaN(numericNewMask)) return false;
-
   return numericNewMask < numericMask;
 }
 
@@ -13,6 +12,7 @@ export function shouldDisableSubmit(
   newMask: string,
   invalidSubnet: boolean
 ): boolean {
+  // .trim() para eliminar espacios en blanco
   return (
     ip.trim() === "" ||
     mask.trim() === "" ||
