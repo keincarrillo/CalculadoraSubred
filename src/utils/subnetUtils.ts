99f1b545e49afef5270ec546c5007ea75ce93881
Kein <kein.carrillo.escom@gmail.com>
2025-05-22 17:42:42 -0600

    refactor<subnetUtils.ts>

--- src/utils/subnetUtils.ts
+++ src/utils/subnetUtils.ts
@@ -10,13 +10,21 @@ export function shouldDisableSubmit(
   ip: string,
   mask: string,
   newMask: string,
-  invalidSubnet: boolean
+  invalidSubnet: boolean,
+  invalidIP: boolean
 ): boolean {
   // .trim() para eliminar espacios en blanco
   return (
     ip.trim() === "" ||
     mask.trim() === "" ||
     newMask.trim() === "" ||
-    invalidSubnet
+    invalidSubnet ||
+    invalidIP
   );
+}
+
+export function isValidIPv4(ip: string): boolean {
+  const ipRegex =
+    /^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3}$/;
+  return ipRegex.test(ip);
 }
